6a858eda58f2fbfb94708b29584e734240d23ec4
mkhan3510 <mkhan3510@richmondhillhs.org>
2020-12-16 18:33:20 +0000

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
+import BusinessList from './Components/BusinessList.js';
+import SearchBar from './Components/SearchBar.js';
+import Yelp from './Components/Yelp';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+class App extends React.Component {
+constructor(props){
+super(props);
+this.state = {
+ businesses:[]
+};
+this.searchYelp = this.searchYelp.bind(this);
+}
+   
+searchYelp(term, location, sortBy){
+   
+
+       Yelp.search(term,location,sortBy)
+       .then(businesses=>{this.setState
+       ({
+               businesses:businesses});
+       });
+    
+   } 
+
+  render() {
+    console.log(this.state.businesses);    
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp = {this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses}/>   
+     </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/BusinessList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './css/BusinessList.css';
+import Business from './Business.js';
+
+
+class BusinessList extends React.Component{
+    
+    render(){
+        return (
+        <div className="BusinessList">
+         {
+            
+            this.props.businesses.map(business=> {   
+            return <Business business={business} key = {business.id} />
+            })
+            
+            
+            
+         }
+            
+            
+           
+          
+      
+        </div>
+        
+        
+        
+        );
+        
+        
+    }
+    
+}
+
+
+export default BusinessList;
